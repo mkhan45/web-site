57c9551bd4ae5ab393defc924154e9fa8feea4d1
Mikail Khan <mikail.khan45@gmail.com>
2019-02-25 11:44:25 -0500

    stuff

--- js/ajax.js
+++ js/ajax.js
@@ -2,22 +2,21 @@ console.log('hi');
 
 $.ajax({
 	url: "kitchen",                      // goes to https://user.tjhsst.edu/pckosek/kitchen
-type: "get",                         // use a 'get' type request
-data:  $('#coord').serialize(), //serialize form and pass to server
-console.log(data);
-success: function(response) {
-	// THIS FUNCTION IS CALLED WHEN KITCHEN IS COMPLETE
-	// -- AND -- everthing went ok
-	// update the display
-	// (bring the order to the customer)
-	$("weather").html(response.weather);
-},
+	type: "get",                         // use a 'get' type request
+	data:  $('#coord').serialize(), //serialize form and pass to server
+	success: function(response) {
+		// THIS FUNCTION IS CALLED WHEN KITCHEN IS COMPLETE
+		// -- AND -- everthing went ok
+		// update the display
+		// (bring the order to the customer)
+		$("weather").html(response.weather);
+	},
 
-error: function (stat, err) {
-	// THIS FUNCTION IS CALLED WHEN KITCHEN IS COMPLETE
-	// -- BUT -- something went wrong (like invalid menu_item)
-	// update the display
-	// (bring the explanation to the customer)
-	$("weather").html("error);
-}
+	error: function (stat, err) {
+		// THIS FUNCTION IS CALLED WHEN KITCHEN IS COMPLETE
+		// -- BUT -- something went wrong (like invalid menu_item)
+		// update the display
+		// (bring the explanation to the customer)
+		$("weather").html("error);
+	}
 });
