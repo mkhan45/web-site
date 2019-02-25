f9ded02ae8e4f274f3cc50ba495fc5d0076a9c28
Mikail Khan <mikail.khan45@gmail.com>
2019-02-25 11:22:40 -0500

    stuff

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,1 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
+console.log('hi');
