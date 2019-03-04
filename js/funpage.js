6bdb37e2cd56895fbab3149b27924d61bf9a018c
Mikail Khan <mikail.khan45@gmail.com>
2019-03-04 14:36:42 -0500

    stuff

--- js/funpage.js
+++ js/funpage.js
@@ -2,3 +2,9 @@ function buttonclick(){
 	body = document.getElementById("body");
 	body.innerHTML = "clicked";
 }
+
+document.onKeyPress = function (e){
+	e = e || window.event;
+
+	console.log(e.keyCode);
+}
