1cbf9c3d270f26bd2f0e55d8ab4eb6170c370ae5
Mikail Khan <mikail.khan45@gmail.com>
2019-03-04 14:53:33 -0500

    stuff

--- js/funpage.js
+++ js/funpage.js
@@ -3,7 +3,12 @@ function buttonclick(){
 	body.innerHTML = "clicked";
 }
 
+var apresses = 0;
 document.onkeypress = function (e){
 	e = e || window.event;
+	if(e.key == "a"){
+		apresses += 1;
+		document.getElementById("updown").innerHTML = "A has been pressed " + apresses + " times.";
+	}
 	console.log(e);
 }
