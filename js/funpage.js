05c23d8d405b576af75efce84492b862a8f846bc
Mikail <mikail.khan45@gmail.com>
2019-03-06 10:37:25 -0500

    funpage done

--- js/funpage.js
+++ js/funpage.js
@@ -1,6 +1,7 @@
 function buttonclick(){
+    searchbar = document.getElementById("bar")
 	body = document.getElementById("body");
-	body.innerHTML = "clicked";
+	body.innerHTML = searchbar.value;
 }
 
 var apresses = 0;
@@ -8,7 +9,7 @@ document.onkeypress = function (e){
 	e = e || window.event;
 	if(e.key == "a"){
 		apresses += 1;
-		document.getElementById("updown").innerHTML = "A has been pressed " + apresses + " times.";
+		document.getElementById("updown").innerHTML = "a has been pressed " + apresses + " times.";
 	}
 	console.log(e);
 }
